4bcd2592c69e7f60d2d0b63125b7f938c0b06431
Jose Abreu <62102444+Disclored@users.noreply.github.com>
2026-08-18 19:01:53 +0100

    Update and rename Remove Anuncios.js to Script - Remove Anuncios.js

--- Script - Remove Anuncios.js
+++ Script - Remove Anuncios.js
@@ -7,7 +7,7 @@
 
 // ==UserScript==
 // @name         Habblet Ads Remover
-// @namespace    http://tampermonkey.net/
+// @namespace    Unknow
 // @version      1.5.1
 // @match        https://www.habblet.city/hotel
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=habblet.city
